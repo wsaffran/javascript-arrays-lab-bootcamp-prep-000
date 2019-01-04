f3b84b86e3376285d395c1a7449554b4560d0d97
William Saffran <willh.saffran@gmail.com>
2019-01-04 23:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function prependKitten(name) {
   return newKittens
 }
 function removeLastKitten() {
-  var newKittens = kitten.slice(0, kittens.length-1)
+  var newKittens = kittens.slice(0, kittens.length-1)
   return newKittens
 }
